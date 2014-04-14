eadb879f45ef8a3aed48b7d4318e80045bf3f9ea
skiwi <frank_skiwi@hotmail.com>
2014-04-14 13:30:52 +0200

    Implemented equals and hashCode.

--- src/com/skiwi/tcg/model/cards/FusionCard.java
+++ src/com/skiwi/tcg/model/cards/FusionCard.java
@@ -1,11 +1,13 @@
 
 package com.skiwi.tcg.model.cards;
 
+import java.util.Objects;
+
 /**
  *
  * @author Frank van Heeswijk
  */
-public class FusionCard implements Card {
+public final class FusionCard implements Card {
     private final String name;
     private final int fusionPower;
     private final int fusionStat;
@@ -27,5 +29,35 @@ public class FusionCard implements Card {
 
     public int getFusionStat() {
         return fusionStat;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + this.fusionPower;
+        hash = 59 * hash + this.fusionStat;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FusionCard other = (FusionCard)obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.fusionPower != other.fusionPower) {
+            return false;
+        }
+        if (this.fusionStat != other.fusionStat) {
+            return false;
+        }
+        return true;
     }
 }
--- src/com/skiwi/tcg/model/cards/MonsterCard.java
+++ src/com/skiwi/tcg/model/cards/MonsterCard.java
@@ -1,11 +1,13 @@
 
 package com.skiwi.tcg.model.cards;
 
+import java.util.Objects;
+
 /**
  *
  * @author Frank van Heeswijk
  */
-public class MonsterCard implements Card {
+public final class MonsterCard implements Card {
     private final String name;
     private final int attack;
     private final int hitpoints;
@@ -27,5 +29,35 @@ public class MonsterCard implements Card {
 
     public int getHitpoints() {
         return hitpoints;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.name);
+        hash = 73 * hash + this.attack;
+        hash = 73 * hash + this.hitpoints;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MonsterCard other = (MonsterCard)obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.attack != other.attack) {
+            return false;
+        }
+        if (this.hitpoints != other.hitpoints) {
+            return false;
+        }
+        return true;
     }
 }
