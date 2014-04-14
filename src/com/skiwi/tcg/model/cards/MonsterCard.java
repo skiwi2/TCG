9679c9290d91fbcdf5903c41351ae3413d98b5ce
skiwi <frank_skiwi@hotmail.com>
2014-04-14 14:00:51 +0200

    Added maximum hitpoints and monster modus to MonsterCard.

--- src/com/skiwi/tcg/model/cards/MonsterCard.java
+++ src/com/skiwi/tcg/model/cards/MonsterCard.java
@@ -1,6 +1,8 @@
 
 package com.skiwi.tcg.model.cards;
 
+import com.skiwi.tcg.model.objects.MonsterModus;
+import com.skiwi.tcg.utils.ExceptionUtils;
 import java.util.Objects;
 
 /**
@@ -10,12 +12,34 @@ import java.util.Objects;
 public final class MonsterCard implements Card {
     private final String name;
     private final int attack;
-    private final int hitpoints;
+    private final int maximumHitpoints;
+    private int hitpoints;
+    private MonsterModus modus;
     
-    public MonsterCard(final String name, final int attack, final int hitpoints) {
+    public MonsterCard(final String name, final int attack, final int hitpoints, final MonsterModus modus) {
         this.name = name;
         this.attack = attack;
+        this.maximumHitpoints = hitpoints;
         this.hitpoints = hitpoints;
+        this.modus = Objects.requireNonNull(modus);
+    }
+    
+    public int increaseHitpoints(final int increment) {
+        ExceptionUtils.throwOnFail(increment >= 0, IllegalArgumentException::new, "increment cannot be negative");
+        int oldHitpoints = hitpoints;
+        hitpoints = (hitpoints + increment >= maximumHitpoints) ? maximumHitpoints : hitpoints + increment;
+        return hitpoints - oldHitpoints;
+    }
+    
+    public int decreaseHitpoints(final int decrement) {
+        ExceptionUtils.throwOnFail(decrement <= 0, IllegalArgumentException::new, "decrement cannot be positive");
+        int oldHitpoints = hitpoints;
+        hitpoints = (hitpoints - decrement <= 0) ? 0 : hitpoints - decrement;
+        return hitpoints - oldHitpoints;
+    }
+    
+    public void setModus(final MonsterModus modus) {
+        this.modus = Objects.requireNonNull(modus);
     }
 
     @Override
@@ -31,12 +55,16 @@ public final class MonsterCard implements Card {
         return hitpoints;
     }
 
+    public MonsterModus getModus() {
+        return modus;
+    }
+
     @Override
     public int hashCode() {
         int hash = 3;
         hash = 73 * hash + Objects.hashCode(this.name);
         hash = 73 * hash + this.attack;
-        hash = 73 * hash + this.hitpoints;
+        hash = 73 * hash + this.maximumHitpoints;
         return hash;
     }
 
@@ -55,7 +83,7 @@ public final class MonsterCard implements Card {
         if (this.attack != other.attack) {
             return false;
         }
-        if (this.hitpoints != other.hitpoints) {
+        if (this.attack != other.maximumHitpoints) {
             return false;
         }
         return true;
