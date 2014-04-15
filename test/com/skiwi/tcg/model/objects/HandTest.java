f50f22e523588903886457feb83067510005f393
skiwi <frank_skiwi@hotmail.com>
2014-04-15 21:29:01 +0200

    Improved consistency of test method naming.

--- test/com/skiwi/tcg/model/objects/HandTest.java
+++ test/com/skiwi/tcg/model/objects/HandTest.java
@@ -14,12 +14,12 @@ public class HandTest {
     {
         assertEquals(true, true);
     }
-    
+
     @Test
     public void testConstructor() {
         Hand hand = new Hand(1);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testConstructorIAE() {
         Hand hand = new Hand(0);
@@ -41,13 +41,13 @@ public class HandTest {
         Hand hand = new Hand(1);
         hand.add(createCard());
     }
-    
+
     @Test(expected = NullPointerException.class)
     public void testAddNPE() {
         Hand hand = new Hand(1);
         hand.add(null);
     }
-    
+
     @Test(expected = IllegalStateException.class)
     public void testAddISE() {
         Hand hand = new Hand(1);
@@ -62,19 +62,19 @@ public class HandTest {
         hand.add(card);
         assertEquals("card should be equal", card, hand.play(0));
     }
-    
+
     @Test(expected = IndexOutOfBoundsException.class)
-    public void testPlayIOOB1() {
+    public void testPlayIOOBE1() {
         Hand hand = new Hand(1);
         hand.play(-1);
     }
-    
+
     @Test(expected = IndexOutOfBoundsException.class)
-    public void testPlayIOOB2() {
+    public void testPlayIOOBE2() {
         Hand hand = new Hand(1);
         hand.play(0);
     }
-    
+
     @Test(expected = IndexOutOfBoundsException.class)
     public void testPlayIOOB3() {
         Hand hand = new Hand(1);
@@ -93,41 +93,41 @@ public class HandTest {
         assertEquals("card should be equal", card, hand.play(1));
         assertEquals("card2 should be equal", card2, hand.play(0));
     }
-    
+
     @Test(expected = IndexOutOfBoundsException.class)
-    public void testSwapIOOB1() {
+    public void testSwapIOOBE1() {
         Hand hand = new Hand(1);
         hand.add(createCard());
         hand.swap(-1, 0);
     }
-    
+
     @Test(expected = IndexOutOfBoundsException.class)
-    public void testSwapIOOB2() {
+    public void testSwapIOOBE2() {
         Hand hand = new Hand(1);
         hand.add(createCard());
         hand.swap(1, 0);
     }
-    
+
     @Test(expected = IndexOutOfBoundsException.class)
-    public void testSwapIOOB3() {
+    public void testSwapIOOBE3() {
         Hand hand = new Hand(1);
         hand.add(createCard());
         hand.swap(0, -1);
     }
-    
+
     @Test(expected = IndexOutOfBoundsException.class)
-    public void testSwapIOOB4() {
+    public void testSwapIOOBE4() {
         Hand hand = new Hand(1);
         hand.add(createCard());
         hand.swap(0, 1);
     }
-    
+
     @Test
     public void testToString1() {
         Hand hand = new Hand(1);
         assertEquals("Hand(1, [])", hand.toString());
     }
-    
+
     @Test
     public void testToString2() {
         Hand hand = new Hand(2);
@@ -137,11 +137,11 @@ public class HandTest {
         hand.add(card2);
         assertEquals("Hand(2, [" + card + ", " + card2 + "])", hand.toString());
     }
-    
+
     private Card createCard() {
         return new MonsterCard("Test", 10, 100, MonsterModus.OFFENSIVE);
     }
-    
+
     private Card createCard2() {
         return new MonsterCard("Test2", 15, 150, MonsterModus.HEALING);
     }
