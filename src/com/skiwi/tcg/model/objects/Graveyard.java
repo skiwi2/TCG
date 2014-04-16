5a5c851e6b6aa98f2d23d68d07b4d493379b3caa
skiwi <frank_skiwi@hotmail.com>
2014-04-16 22:31:34 +0200

    Refactored Deck and Graveyard to both inherit from Pile, closes #27. Graveyard extends Pile and thus implements Collection<Card>, closes #16, closes #20, closes #23. Unit tests added for Graveyard, closes #16. Minor refactoring on author comments in test classes.

--- src/com/skiwi/tcg/model/objects/Graveyard.java
+++ src/com/skiwi/tcg/model/objects/Graveyard.java
@@ -3,33 +3,22 @@ package com.skiwi.tcg.model.objects;
 
 import com.skiwi.tcg.model.cards.Card;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Queue;
 
 /**
  *
  * @author Frank van Heeswijk
  */
-public class Graveyard {
-    private final Queue<Card> queue = new LinkedList<>();
-    
-    public Graveyard() { }
-    
-    public Graveyard(final Collection<Card> cards) {
-        Objects.requireNonNull(cards);
-        queue.addAll(cards);
+public class Graveyard extends Pile {
+    public Graveyard() {
+        super();
     }
     
-    public void add(final Card card) {
-        Objects.requireNonNull(card);
-        queue.add(card);
+    public Graveyard(final Collection<? extends Card> cards) {
+        super(cards);
     }
     
-    public void addAll(final Collection<Card> cards) {
-        Objects.requireNonNull(cards);
-        queue.addAll(cards);
+    @Override
+    public String toString() {
+        return "Graveyard(" + size() + ")";
     }
 }
---
+++ test/com/skiwi/tcg/model/objects/GraveyardTest.java
@@ -0,0 +1,56 @@
+
+package com.skiwi.tcg.model.objects;
+
+import com.skiwi.tcg.model.cards.Card;
+import com.skiwi.tcg.model.cards.MonsterCard;
+import java.util.ArrayList;
+import java.util.Collection;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Frank van Heeswijk
+ */
+public class GraveyardTest {
+    static {
+        assertTrue(true);
+    }
+    
+    @Test
+    public void testConstructor() {
+        new Graveyard();
+    }
+    
+    @Test
+    public void testConstructor_collection() {
+        new Graveyard(createCollection());
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testConstructor_collectionNPE() {
+        new Graveyard(null);
+    }
+    
+    @Test
+    public void testToString() {
+        Graveyard graveyard = new Graveyard();
+        graveyard.addAll(createCollection());
+        assertEquals("Graveyard(2)", graveyard.toString());
+    }
+    
+    private Card createCard() {
+        return new MonsterCard("Test", 10, 100, MonsterModus.OFFENSIVE);
+    }
+
+    private Card createCard2() {
+        return new MonsterCard("Test2", 15, 150, MonsterModus.HEALING);
+    }
+    
+    private Collection<Card> createCollection() {
+        Collection<Card> collection = new ArrayList<>();
+        collection.add(createCard());
+        collection.add(createCard2());
+        return collection;
+    }
+}
