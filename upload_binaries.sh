c5bb24d77750efd156ab7005512a9dd074a3ee3c
skiwi <frank_skiwi@hotmail.com>
2014-05-24 18:31:34 +0200

    Quoted the JAR directory and added a newline at the end of file for the upload binaries script.

--- upload_binaries.sh
+++ upload_binaries.sh
@@ -35,7 +35,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   curl -X POST -H "Authorization: token ${GH_TOKEN}" \
      -H "Accept: application/vnd.github.manifold-preview" \
      -H "Content-Type: application/java-archive" \
-     --data-binary /home/travis/build/{$GH_USER}/{$GH_REPO}/target/TCG-1.0-SNAPSHOT.jar \
+     --data-binary "/home/travis/build/{$GH_USER}/{$GH_REPO}/target/TCG-1.0-SNAPSHOT.jar" \
      "https://uploads.github.com/repos/${GH_USER}/${GH_REPO}/releases/${IDDI}/assets?name=tcg-master-${TRAVIS_BUILD_NUMBER}.jar"
 
   echo -e "Done uploading\n"
