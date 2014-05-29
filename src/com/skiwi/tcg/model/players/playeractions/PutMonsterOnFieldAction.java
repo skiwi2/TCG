cf3212834e7b463b7dda779aa81850e64c784826
skiwi <frank_skiwi@hotmail.com>
2014-05-29 16:08:53 +0200

    Added the PlayerAction in which a player can put a monster on the field, closes #50.

---
+++ src/com/skiwi/tcg/model/players/playeractions/PutMonsterOnFieldAction.java
@@ -0,0 +1,56 @@
+
+package com.skiwi.tcg.model.players.playeractions;
+
+import com.skiwi.tcg.model.cards.MonsterCard;
+import com.skiwi.tcg.model.objects.Field;
+import com.skiwi.tcg.model.objects.Hand;
+import com.skiwi.tcg.model.players.Player;
+import com.skiwi.tcg.model.players.PlayerAction;
+import com.skiwi.tcg.model.players.PlayerActionNotAllowedException;
+import com.skiwi.tcg.utils.Arguments;
+import java.util.Objects;
+
+/**
+ *
+ * @author Frank van Heeswijk
+ */
+public class PutMonsterOnFieldAction implements PlayerAction {
+    private final int handCardIndex;
+    private final int fieldMonsterIndex;
+
+    public PutMonsterOnFieldAction(final int handCardIndex, final int fieldMonsterIndex) {
+        this.handCardIndex = Arguments.requirePositiveOrZero(handCardIndex, "handCardIndex");
+        this.fieldMonsterIndex = Arguments.requirePositiveOrZero(fieldMonsterIndex, "fieldCardIndex");
+    }
+
+    @Override
+    public boolean isActionAllowed(final Player player) {
+        Objects.requireNonNull(player, "player");
+        Hand hand = player.getHand();
+        Field field = player.getField();
+        if (handCardIndex >= hand.getCapacity()) {
+            return false;
+        }
+        if (fieldMonsterIndex >= field.getMonsterCapacity()) {
+            return false;
+        }
+        if (field.hasMonster(fieldMonsterIndex)) {
+            return false;
+        }
+        if (!(hand.get(handCardIndex) instanceof MonsterCard)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void performAction(final Player player) {
+        Objects.requireNonNull(player);
+        if (!isActionAllowed(player)) {
+            throw new PlayerActionNotAllowedException();
+        }
+        Hand hand = player.getHand();
+        Field field = player.getField();
+        field.setMonster(fieldMonsterIndex, (MonsterCard)hand.play(handCardIndex));
+    }
+}
---
+++ test/com/skiwi/tcg/model/players/playeractions/PutMonsterOnFieldActionTest.java
@@ -0,0 +1,159 @@
+
+package com.skiwi.tcg.model.players.playeractions;
+
+import com.skiwi.tcg.model.cards.Card;
+import com.skiwi.tcg.model.cards.FusionCard;
+import com.skiwi.tcg.model.cards.MonsterCard;
+import com.skiwi.tcg.model.objects.Deck;
+import com.skiwi.tcg.model.objects.Field;
+import com.skiwi.tcg.model.objects.FusionStat;
+import com.skiwi.tcg.model.objects.Graveyard;
+import com.skiwi.tcg.model.objects.Hand;
+import com.skiwi.tcg.model.objects.MonsterModus;
+import com.skiwi.tcg.model.players.Player;
+import com.skiwi.tcg.model.players.PlayerActionNotAllowedException;
+import com.skiwi.tcg.model.players.TurnAction;
+import java.util.Collections;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Frank van Heeswijk
+ */
+public class PutMonsterOnFieldActionTest {
+    static {
+        assertTrue(true);
+    }
+    
+    @Test
+    public void testConstructor() {
+        new PutMonsterOnFieldAction(0, 0);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorNegativeHandCardIndex() {
+        new PutMonsterOnFieldAction(-1, 0);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorNgativeFieldMonsterIndex() {
+        new PutMonsterOnFieldAction(0, -1);
+    }
+
+    @Test
+    public void testIsActionAllowed() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        player.getHand().add(createCard());
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 0);
+        assertTrue(putMonsterOnFieldAction.isActionAllowed(player));
+    }
+
+    @Test
+    public void testPerformAction() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        Hand hand = player.getHand();
+        Field field = player.getField();
+        Card card = createCard();
+        Card card2 = createCard();
+        assertNotSame(card, card2);
+        hand.add(card);
+        hand.add(card2);
+        
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 0);
+        putMonsterOnFieldAction.performAction(player);
+        assertEquals(1, hand.size());
+        assertFalse(hand.contains(card));
+        assertEquals(card2, hand.get(0));
+        
+        assertEquals(card, field.getMonster(0));
+        assertEquals(1, field.getMonsters().count());
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testIsActionAllowedNullPlayer() {
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 0);
+        putMonsterOnFieldAction.isActionAllowed(null);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testPerformActionNullPlayer() {
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 0);
+        putMonsterOnFieldAction.performAction(null);
+    }
+    
+    @Test
+    public void testIsActionAllowedHandCardIndexOverCapacity() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(5, 0);
+        assertFalse(putMonsterOnFieldAction.isActionAllowed(player));
+    }
+    
+    @Test(expected = PlayerActionNotAllowedException.class)
+    public void testPerformActionHandCardIndexOverCapacity() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(5, 0);
+        putMonsterOnFieldAction.performAction(player);
+    }
+    
+    @Test
+    public void testIsActionAllowedFieldMonsterIndexOverCapacity() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 5);
+        assertFalse(putMonsterOnFieldAction.isActionAllowed(player));
+    }
+    
+    @Test(expected = PlayerActionNotAllowedException.class)
+    public void testPerformActionFieldMonsterIndexOverCapacity() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 5);
+        putMonsterOnFieldAction.performAction(player);
+    }
+    
+    @Test
+    public void testIsActionAllowedMonsterAlreadyExistsOnField() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        player.getHand().add(createCard());
+        player.getField().setMonster(0, new MonsterCard("Test", 5, 5, MonsterModus.HEALING));
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 0);
+        assertFalse(putMonsterOnFieldAction.isActionAllowed(player));
+    }
+    
+    @Test(expected = PlayerActionNotAllowedException.class)
+    public void testPerformActionMonsterAlreadyExistsOnField() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        player.getHand().add(createCard());
+        player.getField().setMonster(0, new MonsterCard("Test", 5, 5, MonsterModus.HEALING));
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 0);
+        putMonsterOnFieldAction.performAction(player);
+    }
+    
+    @Test
+    public void testIsActionAllowedNoMonsterCardSelected() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        player.getHand().add(new FusionCard("Test", 20, FusionStat.ATTACK));
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 0);
+        assertFalse(putMonsterOnFieldAction.isActionAllowed(player));
+    }
+    
+    @Test(expected = PlayerActionNotAllowedException.class)
+    public void testPerformActionNoMonsterCardSelected() {
+        Player player = new Player("Test", 100, new TurnActionImpl(), new Hand(5), new Field(5), createDeck(10), new Graveyard());
+        player.getHand().add(new FusionCard("Test", 20, FusionStat.ATTACK));
+        PutMonsterOnFieldAction putMonsterOnFieldAction = new PutMonsterOnFieldAction(0, 0);
+        putMonsterOnFieldAction.performAction(player);
+    }
+    
+    private Card createCard() {
+        return new MonsterCard("Test", 5, 5, MonsterModus.HEALING);
+    }
+    
+    private Deck createDeck(final int size) {
+        return Deck.newShuffledDeck(Collections.nCopies(size, createCard()));
+    }
+    
+    private static class TurnActionImpl implements TurnAction {
+        @Override
+        public void performTurn(final Player player) { }
+    }
+}
